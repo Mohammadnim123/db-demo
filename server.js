3a7e42060715d21ebc59d52e992d8bc8ae696afe
Mohammadnim123 <mm.nemrawi96@gmail.com>
2021-04-27 04:44:32 +0300

    done

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const pg = require('pg');
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 8000;
-const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    sslmode: process.env.NODE_ENV === "production" ? "require" : "disable"
+  })
 // const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 // client.connect();
 
